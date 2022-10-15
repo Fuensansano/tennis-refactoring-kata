54cbfc19da025c559316682c3133a6de91f7547b
FuenSansano <fuensanosano@gmail.com>
2022-10-15 17:18:34 +0200

    created setUp method in PlayerTest class

--- php/tests/PlayerTest.php
+++ php/tests/PlayerTest.php
@@ -10,26 +10,29 @@ use TennisGame\Player;
 class PlayerTest extends TestCase
 {
 
+    private Player $player;
+    public const A_RANDOM_PLAYER_NAME = 'A_RANDOM_PLAYER_NAME';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->player = new Player(self::A_RANDOM_PLAYER_NAME);
+    }
+
     public function test_create_player_with_a_name()
     {
-        $name = 'Fuen';
-        $player = new Player($name);
-        $this->assertInstanceOf(Player::class,  $player);
-        $this->assertSame($name, $player->name());
+        $this->assertInstanceOf(Player::class,  $this->player);
+        $this->assertSame(self::A_RANDOM_PLAYER_NAME, $this->player->name());
     }
 
     public function test_have_a_score()
     {
-        $name = 'Fuen';
-        $player = new Player($name);
-        $this->assertSame(0, $player->score());
+        $this->assertSame(0, $this->player->score());
     }
 
     public function test_have_a_increment_score()
     {
-        $name = 'Fuen';
-        $player = new Player($name);
-        $player->increaseScore();
-        $this->assertSame(1, $player->score());
+        $this->player->increaseScore();
+        $this->assertSame(1, $this->player->score());
     }
 }
